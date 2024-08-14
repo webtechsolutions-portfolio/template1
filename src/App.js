b51e030176ddce8de8068446314d654f6f8cbd3b
Zeazelu <zeazelu@gmail.com>
2024-08-14 18:32:27 +0200

    contact form

--- src/App.js
+++ src/App.js
@@ -47,6 +47,56 @@ function App() {
     }
   }, [currentSection]);
 
+  const [selectedHeader, setSelectedHeader] = useState(0);
+
+  const handleHeaderClick = (headerIndex) => {
+    setSelectedHeader(headerIndex);
+  };
+
+  const headers = [
+    {
+      title: "Lorem Ipsum1",
+      content: (
+        <form>
+          <input type="text" placeholder="Lorem Ipsum1" name="user_name" />
+          <input
+            type="email"
+            placeholder="email1@example.com"
+            name="user_email"
+          />
+          <input type="text" placeholder="Lorem Ipsum2" name="user_name" />
+          <input type="text" placeholder="Lorem Ipsum2" name="user_name" />
+          <textarea placeholder="Lorem Ipsum1" name="message" />
+          <div className="contact__button__container">
+            <button type="submit">
+              Wyślij <FaArrowRight />
+            </button>
+          </div>
+        </form>
+      ),
+    },
+    {
+      title: "Lorem Ipsum2",
+      content: (
+        <form>
+          <input type="text" placeholder="Lorem Ipsum2" name="user_name" />
+          <input
+            type="email"
+            placeholder="email2@example.com"
+            name="user_email"
+          />
+          <input type="text" placeholder="Lorem Ipsum2" name="user_name" />
+          <input type="text" placeholder="Lorem Ipsum2" name="user_name" />
+          <textarea placeholder="Lorem Ipsum2" name="message" />
+          <div className="contact__button__container">
+            <button type="submit">
+              Wyślij <FaArrowRight />
+            </button>
+          </div>
+        </form>
+      ),
+    },
+  ];
   return (
     <div className="App">
       <header>
@@ -127,6 +177,39 @@ function App() {
               </h2>
               <h3>email@example.com</h3>
               <h3>+48 123 456 789</h3>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section ref={(el) => (sectionRefs.current[2] = el)}>
+        <div className="contact__container">
+          <div className="contact__wrapper">
+            <h1>Lorem Ipsum</h1>
+            <h2>Lorem Ipsum Lorem Ipsum</h2>
+          </div>
+          <div className="contact__wrapper">
+            <div className="contact__headers">
+              {headers.map((header, index) => (
+                <h3
+                  key={index}
+                  onClick={() => handleHeaderClick(index)}
+                  className={
+                    selectedHeader === index ? "contact__header--selected" : ""
+                  }
+                >
+                  {header.title}
+                </h3>
+              ))}
+            </div>
+            <div className="contact__content">
+              {headers.map(
+                (header, index) =>
+                  selectedHeader === index && (
+                    <p key={index} className="contact__fade-in">
+                      {header.content}
+                    </p>
+                  )
+              )}
             </div>
           </div>
         </div>
